501512ea6ae5acd2f7be35ba7fdaf6cb291fe2ec
ErikSkole <e-erijoh6@iskule.no>
2023-05-16 11:51:32 +0200

    cum

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "/natProsjekt/",
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/natProsjekt/'
+    : '/'
 })
